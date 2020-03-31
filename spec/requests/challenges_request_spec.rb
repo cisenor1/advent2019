dc908ac8f7dffbeb7d25bb5922a156c8bc36af86
Craig <cisenor1@gmail.com>
2020-03-31 16:22:53 -0700

    Add Challenge 1 Request Spec

--- spec/requests/challenges_request_spec.rb
+++ spec/requests/challenges_request_spec.rb
@@ -3,10 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Challenges', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/challenges/index'
+  describe 'POST /challenge1' do
+    let(:headers) do
+      {
+        "ACCEPT" => "application/json",
+        "HTTP_ACCEPT" => "application/json"
+      }
+    end
+    let(:mock_file) { fixture_file_upload('challenge1_mock.txt') }
+    let(:params) { { mass_file: { data: mock_file }} }
+    let(:expected_value) { 33583 + 654 + 2 +2 }
+    let(:expected_body) do
+      {
+        result: {
+          value: expected_value
+        }
+      }.to_json
+    end
+
+    it 'returns the expected result' do
+      post '/challenge1', params: params, headers: headers
       expect(response).to have_http_status(:success)
+      expect(response.body).to eq expected_body
     end
   end
 end
